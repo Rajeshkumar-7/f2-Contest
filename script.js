c5ca0cacc4d0ad5915390deea04ced2a16979f86
Rajesh kumar <rajeshkumar.d190@gmail.com>
2024-01-13 18:23:13 +0530

    Solution Added

--- script.js
+++ script.js
@@ -8,20 +8,38 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.filter(person => person.profession === "developer")
+    .map(developer => console.log(developer.name));
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(person => {
+    if (person.profession === "developer") {
+      console.log(person.name);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({ id: 4, name: "susan", age: "20", profession: "intern" });
+  console.log("Data added:", arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter(person => person.profession !== "admin");
+  console.log("Admin removed:", arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArray = [
+  ];
+
+  arr = arr.concat(newArray);
+  console.log("Concatenated array:", arr);
 }
+
+
